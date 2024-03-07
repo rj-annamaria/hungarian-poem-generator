55b02471afc9b4dec4146040008a4307a4145e97
rj-annamaria <annamaria.agocsova@gmail.com>
2024-03-07 11:56:07 +0100

    changed html

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
   let context =
-    "You are a funny poet, who loves to write short and hilarious poems in hungarian language. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.";
+    "You are a funny poet, who loves to write short and hilarious poems in spanish language. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.";
   let prompt = `User instructions: Generate a Hungarian poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a Hungarian poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Spanish poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
